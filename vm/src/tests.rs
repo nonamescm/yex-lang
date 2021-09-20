5bbb3847825a9c13b034e76f20dc0b2e6caa7243
Murilo Menezes <snadragona12bionic@gmail.com>
2021-09-20 14:48:41 -0300

    add other tests

--- vm/src/tests.rs
+++ vm/src/tests.rs
@@ -77,3 +77,20 @@ fn test_string_concat() {
 
     assert_eq!(test, Literal::Str("Hello, World".to_string()));
 }
+
+#[test]
+#[should_panic]
+fn should_panic_string_add_number() {
+    use crate::{Instruction, Literal, VirtualMachine};
+
+    let mut vm = VirtualMachine::default();
+
+    let test = vm.run(vec![
+        Instruction::Load(Literal::Str("Hello, ".to_string()), 40),
+        Instruction::Load(Literal::Num(1.0), 255),
+        Instruction::Add(40, 255, 30),
+        Instruction::Ret(30),
+    ]);
+
+    assert_eq!(test, Literal::Str("Hello, World".to_string()));
+}
