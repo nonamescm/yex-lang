8d5319e804d6f68f4328176b7484e8fe630e5ff6
Murilo Menezes <snadragona12bionic@gmail.com>
2021-10-05 16:13:22 -0300

    add a compiler function as public interface

--- front/src/lib.rs
+++ front/src/lib.rs
@@ -1,5 +1,12 @@
-mod lexer;
-pub mod compiler;
-mod tokens;
+mod compiler;
 mod error;
+mod lexer;
 mod tests;
+mod tokens;
+
+use compiler::Compiler;
+use lexer::Lexer;
+
+pub fn compile<T: Into<String>>(str: T) -> Vec<vm::Instruction> {
+    Compiler::compile(Lexer::new(str)).unwrap()
+}
