a5e70361a168e970f6aa3cedf0df56c4a97003b2
Murilo Menezes <snadragona12bionic@gmail.com>
2021-12-04 11:25:33 -0300

    remove repl

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,5 +1,4 @@
 use front::compile;
-use rustyline::{error::ReadlineError, Editor};
 use std::fs;
 use vm::VirtualMachine;
 
@@ -15,60 +14,9 @@ fn start(mut args: Vec<String>) -> i32 {
         vm.set_consts(constants);
         vm.run(bytecode);
         return 0;
-    }
-
-    let mut repl = Editor::<()>::new();
-    repl.load_history("/tmp/.yex_history").ok();
-
-    loop {
-        match repl.readline("yex> ") {
-            Ok(l) => {
-                let mut l = l.trim().to_string();
-                repl.add_history_entry(&l);
-                if l == "" {
-                    continue;
-                }
-                if !l.starts_with("let") {
-                    l = String::from("let __last__ = puts(") + &l;
-                    l.push(')');
-                }
-                match compile(l) {
-                    Ok((bytecode, constants)) => {
-                        #[cfg(debug_assertions)]
-                        eprintln!(
-                            "instructions: {:#?}\nconstants: {:#?}\n",
-                            bytecode.iter().map(|c| c.opcode).collect::<Vec<_>>(),
-                            constants
-                        );
-                        vm.set_consts(constants);
-                        vm.run(bytecode);
-                        vm.debug_stack();
-
-                        if !vm.pop_last().is_nil() {
-                            println!("=> {}", vm.pop_last())
-                        }
-                    }
-                    Err(e) => println!("{}", e),
-                }
-            }
-            Err(ReadlineError::Interrupted) => {
-                continue;
-            }
-
-            Err(ReadlineError::Eof) => {
-                break 0;
-            }
-
-            Err(e) => {
-                println!("error reading line");
-
-                #[cfg(debug_assertions)]
-                eprintln!("Error: {}", e);
-
-                drop(e);
-            }
-        }
-        vm.reset();
+    } else {
+        println!("Yex language interpreter");
+        1
     }
 }
 
