6b4d778cf072bec69c40359f13601a26a4f0a1c4
Murilo Menezes <snadragona12bionic@gmail.com>
2021-12-05 10:50:54 -0300

    fix cli panicking

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,12 +1,17 @@
 use front::compile;
 use std::fs;
+use std::{env::args, process::exit};
 use vm::VirtualMachine;
 
 fn start(mut args: Vec<String>) -> i32 {
     let mut vm = VirtualMachine::default();
 
     if args.len() > 1 {
-        let file = fs::read_to_string(std::mem::take(&mut args[1])).unwrap();
+        let file = fs::read_to_string(std::mem::take(&mut args[1])).unwrap_or_else(|_| {
+            eprintln!("file not found");
+            exit(1)
+        });
+
         let (bytecode, constants) = compile(file).unwrap_or_else(|e| {
             println!("{}", e);
             (vec![], vec![])
@@ -26,7 +31,5 @@ fn start(mut args: Vec<String>) -> i32 {
 }
 
 fn main() {
-    use std::{env::args, process::exit};
-
     exit(start(args().collect()))
 }
