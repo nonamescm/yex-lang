8ede6c411d2d203b0f19562e9e4d0874de1783af
yxqsnz <yxqsnz@gmail.com>
2021-12-14 15:35:24 -0300

    add basic io to prelude, and fix insert_fn macro.

--- vm/src/prelude.rs
+++ vm/src/prelude.rs
@@ -1,6 +1,6 @@
-use crate::{env::Table, Constant};
+use crate::{env::Table, panic, Constant};
+use std::fs;
 use std::io::{self, Write};
-
 fn puts(args: &[Constant]) -> Constant {
     match args[0] {
         Constant::Str(ref s) => println!("{}", s),
@@ -54,7 +54,61 @@ fn tail(args: &[Constant]) -> Constant {
 fn str(args: &[Constant]) -> Constant {
     Constant::Str(format!("{}", &args[0]))
 }
-
+fn create_file(args: &[Constant]) -> Constant {
+    use Constant::*;
+    match &args[0] {
+        Str(ref filename) => {
+            let _ = fs::File::create(filename);
+        }
+        other => panic!("file_create() expected str, found {}", other),
+    }
+    Nil
+}
+fn write_file(args: &[Constant]) -> Constant {
+    use Constant::*;
+    match &args[0] {
+        Str(ref content) => match &args[1] {
+            Str(ref filename) => {
+                let _ = fs::write(filename, content);
+            }
+            other => panic!("file_write()[1] expected str, found {}", other),
+        },
+        other => panic!("file_write() expected str, found {}", other),
+    }
+    Nil
+}
+fn exists_file(args: &[Constant]) -> Constant {
+    use Constant::*;
+    match &args[0] {
+        Str(ref filename) => {
+            if fs::File::open(filename).is_ok() {
+                return Bool(true);
+            }
+        }
+        other => panic!("file_exists() expected str, found {}", other),
+    }
+    Bool(false)
+}
+fn remove_file(args: &[Constant]) -> Constant {
+    use Constant::*;
+    match &args[0] {
+        Str(ref filename) => {
+            let _ = fs::remove_file(filename);
+        }
+        other => panic!("file_remove() expected str, found {}", other),
+    }
+    Nil
+}
+fn read_file(args: &[Constant]) -> Constant {
+    use Constant::*;
+    match &args[0] {
+        Str(filename) => match fs::read_to_string(filename) {
+            Ok(v) => Str(v),
+            Err(_) => Nil,
+        },
+        other => panic!("file_read() str, found {}", other),
+    }
+}
 fn r#type(args: &[Constant]) -> Constant {
     Constant::Str(
         match &args[0] {
@@ -80,7 +134,7 @@ fn int(args: &[Constant]) -> Constant {
     let str = match &args[0] {
         Constant::Sym(symbol) => symbol.to_str(),
         Constant::Str(ref str) => str,
-        other => crate::panic!("Expected a string or a symbol, found {}", other)
+        other => crate::panic!("Expected a string or a symbol, found {}", other),
     };
 
     match str.parse::<f64>() {
@@ -93,13 +147,13 @@ pub fn prelude() -> Table {
     let mut prelude = Table::new();
     macro_rules! insert_fn {
         ($name: expr, $fn: expr) => {
-            insert_fn!($name, $fn, arity: 1)
+            insert_fn!($name, $fn, 1)
         };
-        ($name: expr, $fn: expr, arity: $arity:expr) => {
+        ($name: expr, $fn: expr, $arity:expr) => {
             prelude.insert(
                 $crate::Symbol::new($name),
                 Constant::NativeFun {
-                    arity: 1,
+                    arity: $arity,
                     fp: |it| $fn(&*it),
                 },
             )
@@ -115,6 +169,10 @@ pub fn prelude() -> Table {
     insert_fn!("type", r#type);
     insert_fn!("inspect", inspect);
     insert_fn!("int", int);
-
+    insert_fn!("file_read", read_file);
+    insert_fn!("file_write", write_file, 2);
+    insert_fn!("file_remove", remove_file);
+    insert_fn!("file_create", create_file);
+    insert_fn!("file_exists", exists_file);
     prelude
 }
