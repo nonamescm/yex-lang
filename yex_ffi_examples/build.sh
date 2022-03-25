29db0f24584127145c2a2ec719205fe8a1a5c3a6
nonamescm <snadragona12bionic@gmail.com>
2022-03-25 19:00:07 +0000

    remove the ffi-related directories

--- yex_ffi_examples/build.sh
+++
@@ -1,6 +0,0 @@
-cp ../yex_ffi_lib yex -rv
-clang *.c yex/yex.c -shared -o example.so || gcc *.c yex/yex.c -shared -o example.so 
-
-cargo run --release --features=repl -q examples.yex || yex examples.yex
-rm -rfv yex
-rm -rf *.so
--- yex_ffi_examples/strtoyex.c
+++
@@ -1,4 +0,0 @@
-#include "yex/yex.h"
-yex_string_t c_str() {
-    return yex_init_str("Hello, world!");
-}
