06079e0a9c93b659ad0ef93255f6165f8d946463
nonamescm <snadragona12bionic@gmail.com>
2022-03-28 22:44:26 +0000

    vm(literal): improve instance display implementation

--- vm/src/literal/instance.rs
+++ vm/src/literal/instance.rs
@@ -12,3 +12,17 @@ impl Instance {
         Instance { ty, fields }
     }
 }
+
+impl std::fmt::Display for Instance {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}(", self.ty.name)?;
+        for (len, (_, value)) in self.fields.iter().enumerate() {
+            write!(f, "{}", value)?;
+            if len != self.fields.len() - 1 {
+                write!(f, ", ")?;
+            }
+        }
+
+        write!(f, ")")
+    }
+}
